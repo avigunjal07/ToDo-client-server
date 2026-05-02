a98ebd2258bd227b0e1e131e725c67360fe144b2
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-02 21:24:53 +0530

    index.js updated with simple api methods

--- server/index.js
+++ server/index.js
@@ -1,13 +1,96 @@
-const express = require("express");
-const cors = require("cors");
-
+import express from "express";
+import cors from "cors";
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+const TODO_ITEMS = ["Go for swimming", "Complete the assignment", "Read the book"];
 
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+    return res.json({
+        success: true,
+        message: "Welcome to the API",
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server started"));
+app.get("/hello", (req, res) => {
+    return res.json({
+        success: true,
+        message: "Hello from server",
+    });
+});
+
+app.get("/todos", (req, res) => {
+    return res.json({
+        success: true,
+        data: TODO_ITEMS,
+        message: "TODO items fetched successfully",
+    });
+});
+
+app.post("/todos", (req, res) => {
+    const { todoItem } = req.body;
+
+    if (!todoItem) {
+        return res.json({
+            success: false,
+            message: "TODO item cannot be empty",
+        });
+    }
+    TODO_ITEMS.push(todoItem);
+
+    return res.json({
+        success: true,
+        data: todoItem,
+        message: "TODO item added successfully",
+    });
+});
+
+app.delete("/todos" , (req , res) => {
+    const {todoItem} = req.body;
+    const  itemIndex = TODO_ITEMS.indexOf(todoItem);
+
+    if(itemIndex == -1) {
+        return res.json({
+            success: false,
+            message:"todo item not found",
+        });
+    } else {
+        TODO_ITEMS.splice(itemIndex , 1);
+
+        return res.json({
+            success: true,
+            data: todoItem , 
+            message:"TODO item deleted successfully",
+        });
+    }
+});
+
+app.put("/todos" , (req , res) => {
+    const {oldTodoItem , newTodoItem} = req.body;
+
+    const itemIndex = TODO_ITEMS.indexOf(oldTodoItem);
+
+    if(itemIndex == -1) {
+        return res.json({
+            success: false ,
+            message: "oldTodoItem not found",
+        });
+    } else {
+        TODO_ITEMS[itemIndex] = newTodoItem;
+
+        return res.json({
+            success:true ,
+            data: newTodoItem ,
+            message: "Todo item added successfully",
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
